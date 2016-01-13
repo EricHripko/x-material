e6198e6e81d7e1ae781511e5768588b173442ea4
Eric Hripko <yksirius@gmail.com>
2016-01-13 14:51:23 +0000

    More work on m-text-field control

--- js/x-material-text-field.js
+++ js/x-material-text-field.js
@@ -7,14 +7,21 @@ xtag.register("m-text-field", {
             this.labelView = document.createElement("m-text-view");
             this.labelView.textStyle = "subheading";
             this.labelView.classList.add("hint");
-            this.labelView.style.color = theme.textSecondary;
+            this.defaultBorder = document.createElement("div");
+            this.defaultBorder.classList.add("border-default");
+            this.activeBorder = document.createElement("div");
+            this.activeBorder.classList.add("border-active");
             // Insert it
             this.innerText = "";
             this.appendChild(this.labelView);
             this.appendChild(this.textView);
+            this.appendChild(this.defaultBorder);
+            this.appendChild(this.activeBorder);
             // Setup styles
             this.textView.style.color = theme.text;
             this.labelView.style.color = theme.textSecondary;
+            this.defaultBorder.style.borderBottomColor = theme.divider;
+            this.activeBorder.style.borderBottomColor = theme.divider;
 
             this.collapsed = false;
         }
@@ -62,6 +69,10 @@ xtag.register("m-text-field", {
     },
     methods: {
         render: function () {
+            this.activeBorder.style.borderBottomColor = hasValue(this.themeColor)
+                ? colors[this.themeColor][500]
+                : theme.divider;
+
             if (this.collapsed) {
                 // Collapse the label
                 this.labelView.textStyle = "caption";
@@ -69,19 +80,33 @@ xtag.register("m-text-field", {
                 if(hasValue(this.themeColor))
                     this.labelView.style.color = colors[this.themeColor][500];
 
+                // Display the outline
+                this.activeBorder.classList.add("shown");
+
                 // Display the input
                 this.textView.classList.add("visible");
                 return;
             }
 
+            // Expand the label
             this.labelView.textStyle = "subheading";
             this.labelView.classList.remove("collapsed");
-            this.labelView.style.removeProperty("color");
+            this.labelView.style.color = theme.textSecondary;
+
+            // Hide the outline
+            this.activeBorder.classList.remove("shown");
+
+            // Hide the input
+            this.textView.classList.remove("visible");
         }
     },
     events: {
         "focus": function () {
             this.collapsed = true;
+            this.render();
+        },
+        "blur": function () {
+            this.collapsed = this.text != "";
             this.render();
         }
     }
