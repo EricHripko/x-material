1a7131b6ac1fa77715c8d2dff3d40d74db6e6697
EricHripko <yksirius@gmail.com>
2016-01-17 12:30:36 +0000

    Fix order of scripts
    Specify core script to be the first one minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var minifyCSS = require("gulp-minify-css");
+var uglify = require("gulp-uglify");
 
-gulp.task('default', function() {
-    gulp.src('styles/*.css')
+gulp.task("default", function() {
+    gulp.src("styles/*.css")
         .pipe(minifyCSS())
-        .pipe(concat('x-material.min.css'))
-        .pipe(gulp.dest('dist/styles'));
-    gulp.src('js/m-*.js')
-        .pipe(concat('x-material.min.js'))
+        .pipe(concat("x-material.min.css"))
+        .pipe(gulp.dest("dist/styles"));
+    gulp.src(["js/m-core.js", "js/m-*.js"])
+        .pipe(concat("x-material.min.js"))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest("dist/js"));
 });
