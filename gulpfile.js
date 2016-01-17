f24ce26e5708f08e5854c895e61fa5b4b6f6bcee
EricHripko <yksirius@gmail.com>
2016-01-17 12:22:52 +0000

    Add build system
    Use Gulp to minify and concat styles and scripts

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', function() {
+    gulp.src('styles/*.css')
+        .pipe(minifyCSS())
+        .pipe(concat('x-material.min.css'))
+        .pipe(gulp.dest('dist/styles'));
+    gulp.src('js/m-*.js')
+        .pipe(concat('x-material.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
